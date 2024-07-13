770af986da7f106d5cafa50f3d65269829eaad09
5756MH <52780995+5756MH@users.noreply.github.com>
2024-07-13 20:30:52 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -825,18 +825,18 @@ const generateRemark = (index, port) => {
     switch (index) {
         case 0:
         case 1:
-            remark = `💦 BPB - Domain_${index + 1} : ${port}`;
+            remark = `🙃 Mohadese-CF SNI Domain tlshello Fragment- Domain_${index + 1} : ${port}`;
             break;
         case 2:
         case 3:
-            remark = `💦 BPB - IPv4_${index - 1} : ${port}`;
+            remark = `💦 MHDS - IPv4_${index - 1} : ${port}`;
             break;
         case 4:
         case 5:
-            remark = `💦 BPB - IPv6_${index - 3} : ${port}`;
+            remark = `🐸 MHDS - IPv6_${index - 3} : ${port}`;
             break;
         default:
-            remark = `💦 BPB - Clean IP_${index - 5} : ${port}`;
+            remark = `🐐 MHDS - Clean IP_${index - 5} : ${port}`;
             break;
     }
 
